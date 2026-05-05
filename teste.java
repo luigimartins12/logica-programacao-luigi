670257bbe36dc37f93d1ee657c20b9cefc137937
luigimartins12 <luigimartinsdarocha123@gmail.com>
2026-05-05 11:25:23 -0300

    Add files via upload

--- teste.java
+++ teste.java
@@ -1,24 +1,32 @@
+package teste;
+
 import java.util.Scanner;
 
 public class teste {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        System.out.println("Qual é o seu nome?");
-        String nome = leitor.nextLine();
-        System.out.println("Qual é a sua idade?");
-        int idade = leitor.nextInt();
-        System.out.println("Qual é a sua altura?");
-        double altura = leitor.nextDouble();
-        System.out.println("Qual o seu peso?");
-        double peso = leitor.nextDouble();
+        System.out.println("=== RPG: BOSS ESPECIAL ===");
+        System.out.println("Um BOSS raro pode surgir se certas condições forem atendidas!\n");
+        System.out.println("1 - O jogador deve estar na área em que o BOSS surge");
+        System.out.println("2 - O jogador deve estar pelo menos no nível de personagem 50");
+        System.out.println("3 - O jogador deve estar com 50% da vida ou menos\n");
 
-        double imc = peso/(altura*altura);
-        System.out.println("\n***DADOS DO USUÁRIO***");
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade + " anos");
-        System.out.println("Altura: " + altura + " metros");
-        System.out.println("Peso: " + peso + "kgs");
-        System.out.println("IMC: " + imc);
+        System.out.println("MARQUE CONDIÇÕES\n");
+
+        System.out.println("O jogador está na área do BOSS? (s para sim, n para não)");
+        String area = leitor.nextLine();
+
+        System.out.println("Em que nível o personagem do jogador está?");
+        int nivel = leitor.nextInt();
+
+        System.out.println("Qual é a porcentagem da vida atual do jogador?");
+        double vida = leitor.nextDouble();
+
+        if (area.equalsIgnoreCase("s") && nivel >= 50 && vida <= 50) {
+            System.out.println("\n=== O INIMIGO ESPECIAL SURGIU! ===");
+        } else {
+            System.out.println("\n=== Nada acontece... ===");
+        }
         leitor.close();
     }
 }
